f19c71b66dc44d31edd6f275a94d540a3b610562
tjuandev <tpydeveloper@gmail.com>
2023-10-03 22:32:21 -0300

    chore: removed unnecessary suspense

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from 'ui/routes'
 import MainProvider from 'ui/providers/mainProvider'
 
 const App = () => (
-  <Suspense fallback={<h1>Loading...</h1>}>
-    <MainProvider>
-      <RouterProvider router={router} />
-    </MainProvider>
-  </Suspense>
+  <MainProvider>
+    <RouterProvider router={router} />
+  </MainProvider>
 )
 
 export default App
