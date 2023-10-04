e2590489ea3d3f81f1320119ef05db18555e97eb
tjuandev <tpydeveloper@gmail.com>
2023-10-03 22:51:22 -0300

    feat: added chakra ui provider

--- src/ui/providers/mainProvider/index.tsx
+++ src/ui/providers/mainProvider/index.tsx
@@ -1,5 +1,8 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import type { MainProviderProps } from './types'
 
-const MainProvider = ({ children }: MainProviderProps) => <>{children}</>
+const MainProvider = ({ children }: MainProviderProps) => (
+  <ChakraProvider>{children}</ChakraProvider>
+)
 
 export default MainProvider
